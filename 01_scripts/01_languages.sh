c7c634487d29f9739d5d7adadb76829972e53392
Sergiu Elmi <srj.elmi@gmail.com>
2022-11-23 22:44:41 +0200

    removed unused scripts and config files

--- 01_scripts/01_languages.sh
+++
@@ -1,5 +0,0 @@
-# pip install python-language-server[all]
-# ln -svf ~/.dotfiles/languages/flake8 ~/.config/flake8
-# ln -svf ~/.dotfiles/languages/pycodestyle ~/.config/pycodestyle
-
-npm i -g javascript-typescript-langserver
