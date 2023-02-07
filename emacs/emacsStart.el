a9a29b7e7a99192da65c211baa99adffe2fd7e1a
Sergiu Elmi <srj.elmi@gmail.com>
2023-02-07 19:20:40 +0200

    removed org-agenda from emacs startup

--- emacs/emacsStart.el
+++ emacs/emacsStart.el
@@ -1,3 +1,4 @@
+;; This was used to start emacs with agenda buffer using emacsclient -c -e "$(cat /path/thisfile)"
 (progn
  ;; (setq org-agenda-skip-function-global '(org-agenda-skip-entry-if 'todo '("BUG" "FEATURE")))
   (org-agenda "" "n" )
