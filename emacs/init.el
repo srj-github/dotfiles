89952bc5fb00fcc3886dd2d4d71d7f6ecd190667
Sergiu Elmi <srj.elmi@gmail.com>
2023-10-12 14:02:39 +0300

    auto installing use-package.

--- emacs/init.el
+++ emacs/init.el
@@ -8,7 +8,15 @@
 ;; .. created at: 2023-02-23
 ;; . . . . . . . . . . . . . . . . . . . . .
 
+(require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-and-compile
+  (setq use-package-always-ensure t
+	use-package-expand-minimally t))
 
 (defun load-directory (directory)
   "Load recursively all `.el' files in DIRECTORY."
